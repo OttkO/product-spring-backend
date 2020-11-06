3a05afccbe06d0e259f1efb2cfedf3e4852d20c8
Adrian Yankov <adrian.yankov@tno.nl>
2020-11-06 13:53:24 +0100

    added get product by ID

--- src/main/java/com/example/demo/controller/ProductController.java
+++ src/main/java/com/example/demo/controller/ProductController.java
@@ -4,8 +4,10 @@ import java.util.List;
 
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -31,5 +33,11 @@ public class ProductController {
 	@PostMapping("/products")
 	public Product addProduct(@RequestBody Product product) {
 		return repository.save(product);	
+	}
+	
+	@GetMapping("/products/{id}")
+	public ResponseEntity<Product> getProduct(@PathVariable Long id) throws Exception {
+		var employee = repository.findById(id).orElseThrow(() -> new Exception("Product does not exist"));
+		return ResponseEntity.ok(employee);
 	}
 }
