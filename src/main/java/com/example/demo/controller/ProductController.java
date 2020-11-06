74cef0b3ba1db46dd706129c7887c4d2e561ef53
Adrian Yankov <adrian.yankov@tno.nl>
2020-11-06 14:24:24 +0100

    added delete

--- src/main/java/com/example/demo/controller/ProductController.java
+++ src/main/java/com/example/demo/controller/ProductController.java
@@ -4,8 +4,10 @@ import java.util.List;
 
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -53,5 +55,11 @@ public class ProductController {
 		var updatedProduct = repository.save(product);
 		
 		return ResponseEntity.ok(updatedProduct);
+	}
+	
+	@DeleteMapping("/products/{id}")
+	public ResponseEntity<Long> deleteProduct(@PathVariable Long id) {
+		repository.deleteById(id);
+		return new ResponseEntity<>(id, HttpStatus.OK);
 	}
 }
